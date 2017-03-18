9a6139259745682f4e191de9c4f011f2e8f3481f
Alma Niu <aniu@scu.edu>
2017-03-18 00:48:25 -0700

    Add files via upload
    -Insert head of linked list

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Hackerank
+//
+//  Created by Alma Niu on 3/18/17.
+//  Copyright © 2017 csciClass. All rights reserved.
+//
+#include <iostream>
+
+
+using namespace std;
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+Node*Insert(Node *head,int data)
+{
+    Node*front=new Node();
+    front->data=data;
+    front->next=head;
+    head=front;
+    return head;
+}
+
+
